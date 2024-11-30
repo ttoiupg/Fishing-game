03e129de1622e80f67847e6c00880a845df8149a
ttoiupg <kopco133596@gmail.com>
2024-11-30 14:44:05 +0800

    add sound and background sound

--- Assets/Scripts/TransitionHandler.cs
+++ Assets/Scripts/TransitionHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuUIHandler : MonoBehaviour
+public class TransitionHandler : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime;
