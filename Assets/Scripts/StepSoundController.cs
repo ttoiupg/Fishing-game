36ff19c473327649fa6d76a99b180bfb5b9d0406
ttoiupg <kopco133596@gmail.com>
2024-12-05 21:54:36 +0800

    audio adjustment and animation fix

--- Assets/Scripts/StepSoundController.cs
+++ Assets/Scripts/StepSoundController.cs
@@ -12,6 +12,6 @@ public class StepSoundController : PlayerSystem
     }
     public void PlayStepSound()
     {
-        SoundFXManger.Instance.PlaySoundFXClip(StepSound, CharacterTransform, 1f);
+        SoundFXManger.Instance.PlaySoundFXClip(StepSound, CharacterTransform, 2f);
     }
 }
