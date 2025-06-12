32ae50dc04208bd278c3133da4b3173102349e27
EricUnity <eric0979028400@gmail.com>
2025-06-12 11:44:31 +0800

    bit configuratino complete

--- Assets/Scripts/Inputs/FishingRodEsp32GamepadHID.cs
+++ Assets/Scripts/Inputs/FishingRodEsp32GamepadHID.cs
@@ -20,24 +20,63 @@ struct FishingRodEsp32GamepadHIDInputReport : IInputStateTypeInfo
     // completeness. This can also help with debugging.
     [FieldOffset(0)] public byte reportId;
 
-    // // The InputControl annotations here probably look a little scary, but what we do
-    // // here is relatively straightforward. The fields we add we annotate with
-    // // [FieldOffset] to force them to the right location, and then we add InputControl
-    // // to attach controls to the fields. Each InputControl attribute can only do one of
-    // // two things: either it adds a new control or it modifies an existing control.
-    // // Given that our layout is based on Gamepad, almost all the controls here are
+    // The InputControl annotations here probably look a little scary, but what we do
+    // here is relatively straightforward. The fields we add we annotate with
+    // [FieldOffset] to force them to the right location, and then we add InputControl
+    // to attach controls to the fields. Each InputControl attribute can only do one of
+    // two things: either it adds a new control or it modifies an existing control.
+    // Given that our layout is based on Gamepad, almost all the controls here are
     // inherited from Gamepad, and we just modify settings on them.
 
-    [InputControl(name = "leftStick", layout = "Stick", format = "VC2B")]
-    [InputControl(name = "leftStick/x", offset = 0, format = "BYTE",
+    [InputControl(name = "leftStick", layout = "Stick", format = "VC2S")]
+    [InputControl(name = "leftStick/x", offset = 1, format = "SHRT",
         parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5")]
-    [InputControl(name = "leftStick/y", offset = 1, format = "BYTE",
+    [InputControl(name = "leftStick/y", offset = 3, format = "SHRT",
         parameters = "invert,normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5")]
     [FieldOffset(1)] public byte leftStickX;
     [FieldOffset(2)] public byte leftStickY;
 
-    [InputControl(name = "buttonSouth", displayName = "buttonSouth", bit = 5)]
-    [FieldOffset(5)] public byte buttons1;
+    //[InputControl(name = "rightStick", layout = "Stick", format = "VC2S")]
+    //[InputControl(name = "rightStick/x", offset = 0, format = "SHRT", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5")]
+    //[InputControl(name = "rightStick/left", offset = 0, format = "BYTE", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5,clamp,clampMin=0,clampMax=0.5,invert")]
+    //[InputControl(name = "rightStick/right", offset = 0, format = "BYTE", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5,clamp,clampMin=0.5,clampMax=1")]
+    //[InputControl(name = "rightStick/y", offset = 1, format = "SHRT", parameters = "invert,normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5")]
+    //[InputControl(name = "rightStick/up", offset = 1, format = "BYTE", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5,clamp,clampMin=0,clampMax=0.5,invert")]
+    //[InputControl(name = "rightStick/down", offset = 1, format = "BYTE", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5,clamp,clampMin=0.5,clampMax=1,invert=false")]
+    //[FieldOffset(3)] public byte rightStickX;
+    //[FieldOffset(4)] public byte rightStickY;
+
+    //[InputControl(name = "dpad", format = "BIT", layout = "Dpad", sizeInBits = 4, defaultState = 8)]
+    //[InputControl(name = "dpad/up", format = "BIT", layout = "DiscreteButton", parameters = "minValue=7,maxValue=1,nullValue=8,wrapAtValue=7", bit = 0, sizeInBits = 4)]
+    //[InputControl(name = "dpad/right", format = "BIT", layout = "DiscreteButton", parameters = "minValue=1,maxValue=3", bit = 0, sizeInBits = 4)]
+    //[InputControl(name = "dpad/down", format = "BIT", layout = "DiscreteButton", parameters = "minValue=3,maxValue=5", bit = 0, sizeInBits = 4)]
+    //[InputControl(name = "dpad/left", format = "BIT", layout = "DiscreteButton", parameters = "minValue=5, maxValue=7", bit = 0, sizeInBits = 4)]
+    //[InputControl(name = "buttonWest", displayName = "Square", bit = 4)]
+    [InputControl(name = "buttonSouth", displayName = "Cross", bit = 8)]
+    //[InputControl(name = "buttonEast", displayName = "Circle", bit = 6)]
+    //[InputControl(name = "buttonNorth", displayName = "Triangle", bit = 7)]
+    [FieldOffset(0)] public byte buttons1;
+
+    //[InputControl(name = "leftShoulder", bit = 0)]
+    //[InputControl(name = "rightShoulder", bit = 1)]
+    //[InputControl(name = "leftTriggerButton", layout = "Button", bit = 2)]
+    //[InputControl(name = "rightTriggerButton", layout = "Button", bit = 3)]
+    //[InputControl(name = "select", displayName = "Share", bit = 4)]
+    //[InputControl(name = "start", displayName = "Options", bit = 5)]
+    //[InputControl(name = "leftStickPress", bit = 6)]
+    //[InputControl(name = "rightStickPress", bit = 7)]
+    //[FieldOffset(6)] public byte buttons2;
+
+    //[InputControl(name = "systemButton", layout = "Button", displayName = "System", bit = 0)]
+    //[InputControl(name = "touchpadButton", layout = "Button", displayName = "Touchpad Press", bit = 1)]
+    //[FieldOffset(7)] public byte buttons3;
+
+    //[InputControl(name = "leftTrigger", format = "BYTE")]
+    //[FieldOffset(8)] public byte leftTrigger;
+
+    //[InputControl(name = "rightTrigger", format = "BYTE")]
+    //[FieldOffset(9)] public byte rightTrigger;
+
     [FieldOffset(30)] public byte batteryLevel;
 }
 
@@ -55,8 +94,8 @@ public class FishingRodGamepad : Gamepad
          InputSystem.RegisterLayout<FishingRodGamepad>(
            matches: new InputDeviceMatcher()
                 .WithInterface("HID")
-                .WithCapability("vendorId", 0x2DC8) // Sony Entertainment.
-                .WithCapability("productId", 0xAB12)); // Wireless controller.
+                .WithCapability("vendorId", 0xe502) // Sony Entertainment.
+                .WithCapability("productId", 0xabcd)); // Wireless controller.
     }
 
     // In the Player, to trigger the calling of the static constructor,
