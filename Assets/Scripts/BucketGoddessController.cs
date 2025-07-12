d78c16373d6b8aef7c640e6504d53d6e42867537
ttoiug <kopco133596@gmail.com>
2025-07-13 00:44:36 +0800

    bucket goddes animation added

--- Assets/Scripts/BucketGoddessController.cs
+++ Assets/Scripts/BucketGoddessController.cs
@@ -1,5 +1,18 @@
+using System;
 using UnityEngine;
 
 public class BucketGoddessController : MonoBehaviour
 {
+    private Animator animator;
+    public GameObject goddessRenderObject;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void TriggerGoddess()
+    {
+        animator.enabled = true;
+    }
 }
